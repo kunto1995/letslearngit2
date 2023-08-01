15828515d1e83d2f2b6ba1b0e0706379d9d07de4
kunto <manjunathvk23@gmail.com>
2023-08-01 10:33:11 +0530

    add number in sum.java

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args){
         int a = 5 ;
-        int b = 10;
+        int b = 1;
         int data = a+b;
         System.out.println(data);
 
